670e22fd9a149ba19c6b6b52665dd9a56af6c155
Roberto García <roberto@rhizomik.net>
2020-05-07 17:46:50 +0200

    Structure as different functions

--- main.py
+++ main.py
@@ -6,14 +6,29 @@ def start_server():
     httpd = HTTPServer(('', 8000), CGIHTTPRequestHandler)
     httpd.serve_forever()
 
-daemon = threading.Thread(name='daemon_server', target=start_server)
-daemon.setDaemon(True)
-daemon.start()
+def run_daemon():
+  daemon = threading.Thread(name='daemon_server', target=start_server)
+  daemon.setDaemon(True)
+  daemon.start()
 
-g = Graph()
-g.parse('http://localhost:8000', format='html')
+def parse_html():
+  g = Graph()
+  g.parse('http://localhost:8000', format='html')
+  return g
 
-print(str(g.serialize(format='turtle')).replace('\\n', '\n'))
-print('\nExtracted triples count: ' + str(len(g)))
+def print_graph(g):
+  import re
+  output = str(g.serialize(format='turtle')).replace('b\'', '').replace('\\n', '\n')
+  output = re.sub(r'(?m)^\@prefix.*\n?', '', output) 
+  print(output)
+  print('\nExtracted triples count: ' + str(len(g)))
 
-assert len(g) > 10, 'The document should include at least 10 triples'
+def at_least_10_triples():
+  run_daemon()
+  g = parse_html()
+  print_graph(g)
+  assert len(g) > 10, 'The document should include at least 10 triples'
+
+if __name__ == "__main__":
+    print('Run HTTP Server on port 8000')
+    start_server()
